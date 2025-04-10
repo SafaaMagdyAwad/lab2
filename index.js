1dc517c4b2b682fc4dd7c867e148568974a06ed3
SafaaMagdyAwad <engsafaamagdy1024@gmail.com>
2025-04-10 19:11:35 +0200

    commit

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 let usersNamesDiv = document.getElementById("usersNames");
 let postsDiv = document.getElementById("posts");
 async function getUsers() {
-
     let users = (await fetch("https://jsonplaceholder.typicode.com/users")).json();
     // console.log(users);
     users.then((users) => {
-        // console.log(users);// append users buttons to usersNamesDiv
+        // console.log(users);
         users.forEach(user => {
             let button = document.createElement("button");
             button.innerText = user.name;
             button.value = user.id;
-
             usersNamesDiv.appendChild(button);
-            button.addEventListener("click", () => {
-                getposts(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`);
+            button.addEventListener("click", (e) => {
+                //select all element with class name selected & remove the class
+                let clickedbuttons=document.querySelectorAll(".clicked");
+                clickedbuttons.forEach((button)=>{
+                    button.classList.remove("clicked");
+                });
+                // console.log(clickedbuttons);
+                e.target.className="clicked",
+                getposts(`https://jsonplaceholder.typicode.com/posts?userId=${e.target.value}`);
             });
         });
     }).catch((er) => {
@@ -26,13 +31,15 @@ async function getposts(url) {
     try {
         postsDiv.innerHTML = "";
         let res = await fetch(url);
-        let posts = await res.json()
-        console.log(posts);
+        let posts = await res.json();
+        // console.log(posts);
+        let postul = document.createElement("ul");
         posts.forEach(post => {
-            console.log(post.title);
-            let postTitle = document.createElement("p");
-            postTitle.innerText = post.title;
-            postsDiv.appendChild(postTitle);
+            // console.log(post.title);
+            let postli=document.createElement("li");
+            postli.innerText = post.title;
+            postul.appendChild(postli);
+            postsDiv.appendChild(postul);
         });
     } catch (e) {
         console.log(e);
